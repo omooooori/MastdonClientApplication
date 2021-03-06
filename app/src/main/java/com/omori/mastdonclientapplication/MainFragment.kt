922e44311385d7b6f1350ee263256cbb2d07bd57
OMORI <r.shimizu18020@gmail.com>
2021-03-06 23:55:39 +0900

    Add onCreateView function

--- app/src/main/java/com/omori/mastdonclientapplication/MainFragment.kt
+++ app/src/main/java/com/omori/mastdonclientapplication/MainFragment.kt
@@ -1,14 +1,22 @@
 package com.omori.mastdonclientapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.omori.mastdonclientapplication.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private var binding: FragmentMainBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
